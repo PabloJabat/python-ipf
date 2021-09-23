59ec10cd0c57126d9c1e33315fe95c027ffbcd0d
PabloJabat <pablojabat@gmail.com>
2021-09-23 11:09:36 +0200

    created IPF class

--- ipf/__main__.py
+++ ipf/__main__.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from . import utils
+from .algorithm import IPF
 
 if __name__ == "__main__":
 
@@ -8,13 +8,8 @@ if __name__ == "__main__":
     target_marginals_1 = np.array([20, 30, 35, 15])
     seed = np.array([[6, 6, 3], [8, 10, 10], [9, 10, 9], [3, 14, 8]])
 
-    result = seed
-    for _ in range(100):
-        new_result = utils.iterate_axis(result, target_marginals_1, 1)
-        new_result = utils.iterate_axis(new_result, target_marginals_0, 0)
+    ipf = IPF(target_marginals_0, target_marginals_1)
 
-        result = new_result
-
-    print(result)
+    print(ipf.run(seed))
 
     print("Done ...")
---
+++ ipf/algorithm.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+class IPF:
+    def __init__(
+        self,
+        target_marginals_0: np.ndarray,
+        target_variables_1: np.ndarray,
+        iterations: int = 50,
+    ) -> None:
+        self.target_marginals_0 = target_marginals_0
+        self.target_marginals_1 = target_variables_1
+        self.iterations = iterations
+
+    def run(self, seed: np.ndarray) -> np.ndarray:
+        """Run IPF"""
+
+        result = seed
+
+        for _ in range(self.iterations):
+            result_0 = _iterate_axis(result, self.target_marginals_0, 0)
+            result_1 = _iterate_axis(result_0, self.target_marginals_1, 1)
+
+            result = result_1
+
+        return result
+
+
+def _iterate_axis(
+    data: np.ndarray, target_marginals: np.ndarray, axis: int
+) -> np.ndarray:
+    """Make an interation on on axis"""
+
+    factor = target_marginals / _compute_marginals(data, axis)
+
+    return _multiply(data, factor, axis=axis)
+
+
+def _multiply(a: np.ndarray, b: np.ndarray, axis: int) -> np.ndarray:
+    """Multiply an array by another one dimensional array across a given axis"""
+
+    assert b.ndim == 1
+
+    dim_array = np.ones((1, a.ndim), int).ravel()
+    dim_array[0 if axis else 1] = -1
+
+    return a * b.reshape(dim_array)
+
+
+def _compute_marginals(data: np.ndarray, axis: int) -> np.ndarray:
+    """Compute the marginal count on one axis"""
+
+    return data.sum(axis=axis)
